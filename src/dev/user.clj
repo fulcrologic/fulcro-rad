5c2a32224da3889136f8fb4fa987b12c9e88eecb
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-11-29 05:33:38 +0100

    Format user.clj
    For readability. I have an opinion about 80 characters per line. I
    will format every file I come across. Is that ok?

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,22 +1,30 @@
 (ns user
   (:require
-    [clojure.pprint :refer [pprint]]
-    [clojure.spec.alpha :as s]
-    [clojure.tools.namespace.repl :as tools-ns :refer [disable-reload! refresh clear set-refresh-dirs]]
-    [com.example.components.middleware]
-    [expound.alpha :as expound]
-    [taoensso.timbre :as log]))
+   [clojure.pprint :refer [pprint]]
+   [clojure.spec.alpha :as s]
+   [clojure.tools.namespace.repl
+    :as tools-ns
+    :refer [disable-reload! refresh clear set-refresh-dirs]]
+   [com.example.components.middleware]
+   [expound.alpha :as expound]
+   [taoensso.timbre :as log]))
 
 (set-refresh-dirs "src/main" "src/test" "src/dev" "src/example")
+
 (alter-var-root #'s/*explain-out* (constantly expound/printer))
+
 (log/merge-config!
   {:level      :info
-   :middleware (if (System/getProperty "dev")
-                 [(fn [{:keys [level vargs] :as data}]
-                    (if (and (= :debug level) (= "=>" (second vargs)))
-                      (update-in data [:vargs 2] #(with-out-str (clojure.pprint/pprint %)))
-                      data))]
-                 [])})
+   :middleware
+   (if (System/getProperty "dev")
+     [(fn [{:keys [level vargs] :as data}]
+        (if (and (= :debug level)
+                 (= "=>" (second vargs)))
+          (update-in data [:vargs 2]
+                     #(with-out-str (clojure.pprint/pprint %)))
+          data))]
+     [])})
+
 (defn in-ns-dev []
   (in-ns 'development)
   :loaded)
@@ -25,7 +33,7 @@
   "Loads all source files and switches to the 'dev' namespace. Throws
   any exceptions that may be thrown during compilation."
   []
-  (let [ret (refresh :after `switch-to-dev)]
+  (let [ret (refresh :after `in-ns-dev)]
     (if (instance? Throwable ret)
       (throw ret)
       ret)))
