d82a3bb73230ed5e53bbf2fe1c6054853c18ff04
Tony Kay <tony.kay@gmail.com>
2021-10-19 14:39:14 -0700

    Dropped cljsjs timezone

--- src/main/com/fulcrologic/rad/type_support/ten_year_timezone.cljs
+++ src/main/com/fulcrologic/rad/type_support/ten_year_timezone.cljs
@@ -1,17 +1,4 @@
-(ns ^:no-doc com.fulcrologic.rad.type-support.ten-year-timezone
-  "NOTE: This is a super-hacky hack to get cljdocs to work. I don't want all of js-joda-timezone because it is
-   HUGE, but if I don't use cljsjs I get no cljdoc. So, I recommend using a js-resolve section in shadow-cljs
-   to re-map it.
-
-   ```
-  {:builds   {:main       {:target            :browser
-                           :output-dir        \"resources/public/js/main\"
-                           :asset-path        \"/js/main\"
-                           :js-options        {:resolve
-                                               {\"js-joda-timezone\"
-                                                {:target  :npm
-                                                :require \"js-joda-timezone/dist/js-joda-timezone-10-year-range.min.js\"}}}}}}
-   ```
-"
+(ns com.fulcrologic.rad.type-support.ten-year-timezone
+  "Loads a 10-year range of time zone definitions"
   (:require
-    [cljsjs.js-joda-timezone]))
+    ["@js-joda/timezone/dist/js-joda-timezone-10-year-range.min.js"]))
